7d0940c3a79ac0ab598030f9fb6a1b6205dec10d
Alberto Esquitino <alberto.esqui2002@gmail.com>
2021-02-28 22:48:03 +0100

    Delete users and create users from admin panel

--- src/components/Admin/Users/ListUsers/ListUsers.js
+++ src/components/Admin/Users/ListUsers/ListUsers.js
@@ -1,12 +1,27 @@
 import React, { useState, useEffect } from 'react'
-import { Switch, List, Avatar, Button, Icon, notification } from 'antd'
+import {
+  Switch,
+  List,
+  Avatar,
+  Button,
+  Icon,
+  Modal as ModalAntd,
+  notification,
+} from 'antd'
 import NoAvatar from '../../../../assets/img/png/no-avatar.png'
 import Modal from '../../../../components/Modal'
 import EditUserForm from '../EditUserForm'
-import { getAvatarApi, activateUserApi } from '../../../../api/user'
+import AddUserForm from '../AddUserForm'
+import {
+  getAvatarApi,
+  activateUserApi,
+  deleteUserApi,
+} from '../../../../api/user'
 import { getAccessTokenApi } from '../../../../api/auth'
 
 import './ListUsers.scss'
+
+const { confirm } = ModalAntd
 
 export default function ListUsers(props) {
   const { usersActive, usersInactive, setReloadUsers } = props
@@ -15,16 +30,32 @@ export default function ListUsers(props) {
   const [modalTitle, setModalTitle] = useState('')
   const [modalContent, setModalContent] = useState(null)
 
+  const addUserModal = () => {
+    setIsVisibleModal(true)
+    setModalTitle('Creando nuevo usuario')
+    setModalContent(
+      <AddUserForm
+        setIsVisibleModal={setIsVisibleModal}
+        setReloadUsers={setReloadUsers}
+      />
+    )
+  }
+
   return (
     <div className="list-users">
-      <div className="list-users__switch">
-        <Switch
-          defaultChecked
-          onChange={() => setViewUsersActives(!viewUsersActives)}
-        />
-        <span>
-          {viewUsersActives ? 'Usuarios Activos' : 'Usuarios inactivos'}
-        </span>
+      <div className="list-users__header">
+        <div className="list-users__header-switch">
+          <Switch
+            defaultChecked
+            onChange={() => setViewUsersActives(!viewUsersActives)}
+          />
+          <span>
+            {viewUsersActives ? 'Usuarios Activos' : 'Usuarios inactivos'}
+          </span>
+        </div>
+        <Button type="primary" onClick={addUserModal}>
+          Nuevo usuario
+        </Button>
       </div>
       {viewUsersActives ? (
         <UsersActive
@@ -120,6 +151,28 @@ function UserActive(props) {
       })
   }
 
+  const showDeleteConfirm = () => {
+    const accessToken = getAccessTokenApi()
+
+    confirm({
+      title: 'Eliminando usuario',
+      content: `¿Estas seguro que quieres eliminar a ${user.email}?`,
+      okText: 'Eliminar',
+      okType: 'danger',
+      cancelText: 'Cancelar',
+      onOk() {
+        deleteUserApi(accessToken, user._id)
+          .then((res) => {
+            notification['success']({ message: res })
+            setReloadUsers(true)
+          })
+          .catch((err) => {
+            notification['error']({ message: err })
+          })
+      },
+    })
+  }
+
   return (
     <List.Item
       actions={[
@@ -129,7 +182,7 @@ function UserActive(props) {
         <Button type="danger" onClick={desactivateUser}>
           <Icon type="stop" />
         </Button>,
-        <Button type="danger" onClick={() => console.log('Eliminar usuario')}>
+        <Button type="danger" onClick={showDeleteConfirm}>
           <Icon type="delete" />
         </Button>,
       ]}
@@ -188,13 +241,35 @@ function UserInactive(props) {
       })
   }
 
+  const showDeleteConfirm = () => {
+    const accessToken = getAccessTokenApi()
+
+    confirm({
+      title: 'Eliminando usuario',
+      content: `¿Estas seguro que quieres eliminar a ${user.email}?`,
+      okText: 'Eliminar',
+      okType: 'danger',
+      cancelText: 'Cancelar',
+      onOk() {
+        deleteUserApi(accessToken, user._id)
+          .then((res) => {
+            notification['success']({ message: res })
+            setReloadUsers(true)
+          })
+          .catch((err) => {
+            notification['error']({ message: err })
+          })
+      },
+    })
+  }
+
   return (
     <List.Item
       actions={[
         <Button type="primary" onClick={activateUser}>
           <Icon type="check" />
         </Button>,
-        <Button type="danger" onClick={() => console.log('Eliminar usuario')}>
+        <Button type="danger" onClick={showDeleteConfirm}>
           <Icon type="delete" />
         </Button>,
       ]}
