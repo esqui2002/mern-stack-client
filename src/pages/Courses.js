bc758f4e637706faf674096d3fa95df90087cf39
Alberto Esquitino <alberto.esqui2002@gmail.com>
2021-03-09 14:54:50 +0100

    Print courses in the website with url and coupons

--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -1,5 +1,42 @@
-import React from 'react'
+import { Col, notification, Row, Spin } from 'antd'
+import React, { useState, useEffect } from 'react'
+import { getCoursesApi } from '../api/course'
+import CoursesList from '../components/Web/Courses/CoursesList'
+import PresentationCourses from '../components/Web/Courses/PresentationCourses'
 
 export default function Courses() {
-  return <div>Courses...</div>
+  const [courses, setCourses] = useState(null)
+
+  useEffect(() => {
+    getCoursesApi()
+      .then((response) => {
+        if (response?.code !== 200) {
+          notification['warning']({ message: response.message })
+        } else {
+          setCourses(response.courses)
+        }
+      })
+      .catch(() => {
+        notification['error']({
+          message: 'Error del servidor, intentelo mas tarde',
+        })
+      })
+  }, [])
+  return (
+    <Row>
+      <Col md={4} />
+      <Col md={16}>
+        <PresentationCourses />
+        {!courses ? (
+          <Spin
+            tip="Cargando cursos"
+            style={{ textAlign: 'center', width: '100%', padding: '20px' }}
+          />
+        ) : (
+          <CoursesList courses={courses} />
+        )}
+      </Col>
+      <Col md={4} />
+    </Row>
+  )
 }
